ccbe467de494474e603cefbb23c5760e4974c323
Thomas Sullivan <thomas.sullivan@protedyne.com>
2025-08-06 15:19:49 -0400

    hydrants.

--- frontend/src/components/MapView.vue
+++ frontend/src/components/MapView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
     import { onMounted, ref, watch } from 'vue'
 
+    import hydrantData from '@/data/hydrants.json';
+
+
     const props = defineProps<{
         lat: number
         lng: number
@@ -10,35 +13,51 @@
     const mapEl = ref<HTMLDivElement | null>(null)
 
     onMounted(() => {
-    if (!window.google?.maps) return
+        if (!window.google?.maps) return
 
-    const center = { lat: props.lat, lng: props.lng }
+        const center = { lat: props.lat, lng: props.lng }
 
-    const map = new google.maps.Map(mapEl.value as HTMLElement, {
-        center,
-        zoom: 17,
-        mapTypeId: 'roadmap',
-    })
+        const hydrantCoords = hydrantData;
 
-    const marker = new google.maps.Marker({
-        position: center,
-        map,
-        title: props.address || 'Selected Location',
-    })
-
-    if (props.address) {
-        const infoWindow = new google.maps.InfoWindow({
-            content: `<div style="font-size:14px;">📍 ${props.address}</div>`,
+        const map = new google.maps.Map(mapEl.value as HTMLElement, {
+            center,
+            zoom: 17,
+            mapTypeId: 'roadmap',
         })
 
-        // // Show the info window immediately
-        // infoWindow.open(map, marker)
-
-        // Show info window on click
-        marker.addListener('click', () => {
-            infoWindow.open(map, marker)
+        const marker = new google.maps.Marker({
+            position: center,
+            map,
+            title: props.address || 'Selected Location',
         })
-    }
+
+        // Now do our hydrant locations
+        hydrantCoords.forEach(coord => {
+            new google.maps.Marker({
+                position: coord,
+                map,
+                title: 'Hydrant',
+                icon: {
+                    url: '/icons/hydrant_32x32.png',
+                    scaledSize: new google.maps.Size(32, 32),
+                }
+            });
+        });
+
+
+        if (props.address) {
+            const infoWindow = new google.maps.InfoWindow({
+                content: `<div style="font-size:14px;">📍 ${props.address}</div>`,
+            })
+
+            // // Show the info window immediately
+            // infoWindow.open(map, marker)
+
+            // Show info window on click
+            marker.addListener('click', () => {
+                infoWindow.open(map, marker)
+            })
+        }
     })
 </script>
 
